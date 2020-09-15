f118d3068686c515e75d7e018085f30b3071b1c3
AI-lxw <1216354571@qq.com>
2020-09-15 11:00:21 +0800

    compile ts Basically completed

--- test.js
+++ test.js
@@ -1,10 +1,17 @@
-let str = `var utils_1 = __webpack_require__("./components/utils");
-"use strict";
+// var partten =/require(?=\()/
+// let str = "require("
+// console.log(str.match(partten));
+const path = require('path')
+let partten =/(?<=require\()(\s*[\"\'](.+?)[\"\']\s*)/g
+content = `
 Object.defineProperty(exports, "__esModule", { value: true });
 var utils_1 = require("./components/utils");
-// import utils from './components/head/utils'
 var dom = utils_1.findDom("title");
-// console.log(utils);
-dom.innerHTML = "hello typescript pack";`
-var pattern =/[\"|'](.*?)[\"|']/gi;
-console.log(str.match(pattern));
+console.log(dom);
+dom.innerHTML = "hello typescript pack";`            
+content = content.replace(partten,(data)=>{
+    data = path.join('js',data.split(/["|']/)[1])
+    data = `'./${data}'`
+    return data
+})
+console.log(content,123456);
